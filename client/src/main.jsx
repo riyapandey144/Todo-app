4d764275edc2b9c0b869773ef6c86147df294dbe
riyapandey144 <pandeyriya2633@gmail.com>
2025-04-22 14:19:37 +0530

    fixed typo

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import UserProvider from "./context/user/userState";
-import TodoProvider from "./context/todos/todoState.jsx";
+import UserProvider from "./context/user/UserState";
+import TodoProvider from "./context/todos/TodoState.jsx";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
